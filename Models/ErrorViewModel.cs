a563450ea4656ebcc578ff5c72f24f1c5cdbd9ab
miguelacaceresrios <miguelacaceresrios@gmail.com>
2025-04-07 15:24:02 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Unidad3UniPiloto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
